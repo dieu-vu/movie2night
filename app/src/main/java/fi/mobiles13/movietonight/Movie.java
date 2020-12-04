2448f3bb1ea5d4a73fc52e59ccc8c2a8521c2ec5
Dieu Vu <dieu.vu@metropolia.fi>
2020-12-04 20:22:39 +0200

    update before pull

--- app/src/main/java/fi/mobiles13/movietonight/Movie.java
+++ app/src/main/java/fi/mobiles13/movietonight/Movie.java
@@ -1,5 +1,18 @@
 package fi.mobiles13.movietonight;
 
 public class Movie {
+    private String name;
+    private String genre;
+    private boolean isAdult;
+    private int year;
+    private double rating;
+
+    public Movie (String name, String genre, boolean isAdult, int year, double rating){
+        this.name = name;
+        this.genre = genre;
+        this.isAdult = isAdult;
+        this.year = year;
+        this.rating = rating;
+    }
 
 }
